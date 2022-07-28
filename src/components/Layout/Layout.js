2ce46bb0c785bac1fca02cd308b5ba1561652663
ogutdgn <ogutdgn@gmail.com>
2022-07-28 14:36:53 +0300

    .

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,7 +8,7 @@ import { getPageTitleByRoute } from '../Urls';
 
 const Layout = ({ children, error }) => {
   
-  const winHeight = window.innerHeight * 0.83
+  const winHeight = window.innerHeight * 0.85
 
 
   return (
